726ca14144ca9b409f77fa7fb395a19dfa2aa27e
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-11-27 20:12:38 +0900

    same url -> router.route().get.post

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
-// GET/ 라우터
-router.get('/', (req, res) => {
-  res.send('Hello, Express');
-});
+// router.get('/abc', (req, res) => {
+//   res.send('GET /abc');
+// });
+
+// router.post('/abc', (req, res) => {
+//   res.send('POST /abc');
+// });
+
+// 같은 주소로 post, get 요청 시 코드 간결하게 
+
+router.route('/abc')
+  .get((req, res) => {
+    res.send('GET /abc');
+  })
+  .post((req, res) => {
+    res.send('POST /abc');
+  });
 
 module.exports = router;
