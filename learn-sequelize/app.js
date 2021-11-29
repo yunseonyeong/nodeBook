1abb70c1c8bf0ddfcc14a124903b978c9486ef66
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-11-30 05:49:19 +0900

    feat : connect routers(index, users, comments)

--- learn-sequelize/app.js
+++ learn-sequelize/app.js
@@ -5,6 +5,9 @@ const nunjucks = require('nunjucks');
 
 const { sequelize } = require('./models');    // models/index.js 에서 export한 db(우리가 만든 User, Comment 모델 정보 담긴) 
                                               // 익스프레스 앱과 연결하기 위해 require
+const indexRouter = require('./routes');
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
 
 const app = express();
 app.set('port', process.env.PORT || 3001);
@@ -25,6 +28,10 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
