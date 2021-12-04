b0511b8741ae2893d2efa6b2941d5f2b6889b8d8
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-12-05 04:27:57 +0900

    feat : create User Model

---
+++ nodebird/models/user.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init({
+      email: {
+        type: Sequelize.STRING(40),
+        allowNull: true,
+        unique: true,
+      },
+      nick: {
+        type: Sequelize.STRING(15),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: true,
+      },
+      provider: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+        defaultValue: 'local',        // kakao 로그인 했을 시, provider : 'kakao', eefault는 local(로컬 로그인)
+      },
+      snsId: {
+        type: Sequelize.STRING(30),   // kakao 로그인 시, snsId 저장
+        allowNull: true,
+      },
+    }, {
+      sequelize,
+      timestamps: true,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: true,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  static associate(db) {}
+};
