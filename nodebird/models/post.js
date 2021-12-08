eb215957060c2f510f55f1e58940bd6c4fb245a7
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-12-09 03:51:52 +0900

    refac : typo

--- nodebird/models/post.js
+++ nodebird/models/post.js
@@ -25,7 +25,7 @@ module.exports = class Post extends Sequelize.Model {
 
   static associate(db) {                // 나중에 sequelize가 알아서 게시글 등록자 id 값 담은 컬럼 생성해준다. 외래키로, 
     db.Post.belongsTo(db.User);         // Post(N) : User(1), 시퀄라이즈가 Post 모델에 UserId 컬럼 추가한다.
-    db.Post.belongToMany(db.Hashtag, { through: 'PostHashtag'});        // Post(N) : Hashtag(M) , PostHashtag 는 중간 모델, 이 때는 User모델과 다르게 
+    db.Post.belongsToMany(db.Hashtag, { through: 'PostHashtag'});        // Post(N) : Hashtag(M) , PostHashtag 는 중간 모델, 이 때는 User모델과 다르게 
                                                                       // 같은 테이블이 아니기 때문에, foreignKey는 자동으로 postId 와 HashtagId 가 추가된다.
   }         
 };
