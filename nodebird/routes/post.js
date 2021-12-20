9f09157cad6119add222e8c46d0a78e491066cd1
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-12-21 02:20:43 +0900

    fix typo

--- nodebird/routes/post.js
+++ nodebird/routes/post.js
@@ -41,7 +41,7 @@ router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
       img: req.body.url,
       UserId: req.user.id,
     });
-    const hashtags = req.body.console.match(/#[^\s#]+/g);
+    const hashtags = req.body.content.match(/#[^\s#]+/g);
     if (hashtags) {
       const result = await Promise.all(
         hashtags.map(tag => {
